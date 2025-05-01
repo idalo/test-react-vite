3a1e50a5bd36c9a752804fa6ab00c89dd3c9bbb7
idalo <ivandariolombardi@gmail.com>
2025-05-01 18:44:06 -0500

    Aplicacion inicial

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <article>
+        <p>Clase DWFS</p>
+      </article>
+      <section>
+        <p>23 de abril</p>
+      </section>
+      <footer>
+        <p>2005</p>
+      </footer>
+    </>
+  )
+}
+
+export default App
