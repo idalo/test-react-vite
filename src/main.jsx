b63da5dcfcf4d9ebbd176813c17e8502118d2bfb
idalo <ivandariolombardi@gmail.com>
2025-05-12 19:05:28 -0500

    Edit component import UsersApp

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/style.css'
-import { UsersApp } from './usersApp'
+import { UsersApp } from './UsersApp'
+
 
 
 
